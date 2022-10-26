07c52851c91cf1c5018382e1826e0a13839727ef
Johannes Reding <johannes.reding@uni-rostock.de>
2022-10-26 16:26:28 +0200

    add missing __init__.py in subdirectory 'dielectric_model'

--- tests/test_ngsolve_model.py
+++ tests/test_ngsolve_model.py
@@ -7,7 +7,7 @@ import ngsolve
 def test_ngsolve_model():
     geometry = SimpleGeometry()
     boundaries = {"contact": 1.0, "wire": 0.0}
-    conductivities = {"saline" : 1278*1e-6/1e-2,}
+    conductivities = {"saline": 1278*1e-6/1e-2}
 
     mesh = Mesh(geometry=geometry, order=2, boundaries=boundaries)
     model = VolumeConductor(mesh=mesh, conductivity=conductivities)
