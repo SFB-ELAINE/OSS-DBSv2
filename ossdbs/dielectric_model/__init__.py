2df9217ce05bdad6f41dcc8ccc1730e00ec5dd4d
Johannes Reding <johannes.reding@uni-rostock.de>
2023-01-03 13:57:35 +0100

    Fixed import path
    Former-commit-id: a4fd2b2d793755042b7c0e54e86134126d95b451

--- ossdbs/dielectric_model/__init__.py
+++ ossdbs/dielectric_model/__init__.py
@@ -1,6 +1,8 @@
 from ossdbs.dielectric_model.dielectric_model import DielectricModel
 from ossdbs.dielectric_model.dielectric_model import DielectricParamters
+from ossdbs.dielectric_model.dielectric_model_var1 import ColeColeFourModelCreator
 
 __all__ = ('DielectricModel',
            'DielectricParamters',
+           'ColeColeFourModelCreator',
            )
