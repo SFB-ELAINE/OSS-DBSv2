f86f48d5b19b7ad8b41b8a45853ef266e0757193
Tom Reincke <Tom Reincke>
2023-01-07 21:40:23 +0100

    Update TestAbbottStJudeActiveTip6142_6145 test class
    Former-commit-id: 6fc98d39cb14a27eebf9be49341b48ccba04629d

--- tests/test_abbottstjudeactivetip6142.py
+++ tests/test_abbottstjudeactivetip6142.py
@@ -1,18 +1,22 @@
-
-
 from ossdbs.electrodes.abbott_stjude_active_tip_6142_6145 import AbbottStjudeActiveTip6142_6145
 import pytest
+from electrode_testbase import ElectrodeTestBase
 
 
 @pytest.mark.skip
-class TestAbbottStJudeeActiveTip6142_6145:
+class TestAbbottStJudeeActiveTip6142_6145(ElectrodeTestBase):
 
     TESTDATA = [
         ((0, 0, 0), (0, 0, 1), 0.0, 'case1.json'),
         ((0, 0, 0), (0, 0, 0), 0.0, 'case1.json'),
         ((0, 0, 0), (0, 0, 1), 3.0, 'case1.json'),
         ((1, -2, 3), (0, 0, 1), 0.0, 'case2.json'),
-        ((1, -2, 3), (0, 0, 1), 0.0, 'case3.json'),
+        ((1, -2, 3), (0, 0, 0), 0.0, 'case2.json'),
+        ((1, -2, 3), (0, 0, 0), 3.0, 'case2.json'),
+        ((1, -2, 3), (2.0, 0, 1.0), 0.0, 'case3.json'), 
+        # Behaviour depends on python handling the floating point operations
+        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 0.0, 'case3.json'),
+        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 3.0, 'case3.json'),
     ]
 
     @pytest.mark.parametrize('translation, direction, rotation, path',
@@ -21,7 +25,7 @@ class TestAbbottStJudeeActiveTip6142_6145:
                                translation,
                                direction,
                                rotation,
-                               path):
+                               path) -> None:
         electrode = AbbottStjudeActiveTip6142_6145(translation,
                                                    direction,
                                                    rotation)
