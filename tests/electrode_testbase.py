69c990c2c72e797156affb4c52e6c96f8a8e1b40
Tom Reincke <Tom Reincke>
2023-01-07 21:46:43 +0100

    Adding load_json functionality to base class
    Former-commit-id: 1afbabd37665e64b68b237a618e0867fb5c6c785

--- tests/electrode_testbase.py
+++ tests/electrode_testbase.py
@@ -8,4 +8,8 @@ class ElectrodeTestBase():
     def electrode_to_dict(self, geometry:netgen.libngpy._NgOCC.TopoDS_Shape):
         # Encode dataclasses as string and decode it as dict
         return json.loads(json.dumps(convert_solid(geometry),cls=EnhancedJSONEncoder))
+    def load_json(self, path: str)-> dict:
+        with open(path, "r") as file:
+            geometry_dict=json.load(file)
+        return geometry_dict
         
