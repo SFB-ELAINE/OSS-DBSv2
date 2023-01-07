b632bd5c77a5a4a73871f9f5b46a62f028f4103b
Tom Reincke <Tom Reincke>
2023-01-08 00:12:59 +0100

    Adding negative tests
    Former-commit-id: 61a7205ca74fd55c9eab9c1dde3ffb700e05fdb8

--- tests/test_abbottstjudeactivetip6142.py
+++ tests/test_abbottstjudeactivetip6142.py
@@ -3,38 +3,59 @@ import pytest
 from tests.electrode_testbase import ElectrodeTestBase
 
 
-@pytest.mark.skip
+#@pytest.mark.skip
 class TestAbbottStJudeActiveTip6142_6145(ElectrodeTestBase):
-    FILE_PREFIX="AbbottStjudeActiveTip6142_6145"
+    FILE_PREFIX="tests/test_data/AbbottStjudeActiveTip6142_6145"
     TESTDATA = [
-        ((0, 0, 0), (0, 0, 1), 0.0,FILE_PREFIX+ '_1.json'),
-        ((0, 0, 0), (0, 0, 0), 0.0,FILE_PREFIX+ '_1.json'),
+        ((0, 0, 0), (0, 0, 1), 0.0,FILE_PREFIX+ '_0.json'),
+        ((0, 0, 0), (0, 0, 0), 0.0,FILE_PREFIX+ '_0.json'),
+        ((0, 0, 0), (0, 0, 1), 3.0,FILE_PREFIX+ '_0.json'),
         ((0, 0, 0), (0, 0, 1), 3.0,FILE_PREFIX+ '_1.json'),
-        ((1, -2, 3), (0, 0, 1), 0.0,FILE_PREFIX+ '_2.json'),
-        ((1, -2, 3), (0, 0, 0), 0.0,FILE_PREFIX+ '_2.json'),
-        ((1, -2, 3), (0, 0, 0), 3.0,FILE_PREFIX+ '_2.json'),
-        ((1, -2, 3), (2.0, 0, 1.0), 0.0,FILE_PREFIX+'_3.json'), 
+        ((1, -2, 3), (0, 0, 1), 0.0,FILE_PREFIX+ '_1.json'),
+        ((1, -2, 3), (0, 0, 0), 0.0,FILE_PREFIX+ '_1.json'),
+        ((1, -2, 3), (0, 0, 0), 3.0,FILE_PREFIX+ '_1.json'),
+        ((1, -2, 3), (2.0, 0, 1.0), 0.0,FILE_PREFIX+'_2.json'), 
         # Behaviour depends on python handling the floating point operations
-        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 0.0,FILE_PREFIX+'_3.json'),
-        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 3.0,FILE_PREFIX+'_3.json'),
+        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 0.0,FILE_PREFIX+'_2.json'),
+        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 3.0,FILE_PREFIX+'_2.json'),
     ]
+    WRONG_TESTDATA = [
+        ((0, 0, -1), (0, 0, 0), 0.0,FILE_PREFIX+ '_0.json'),
+        ((0, 0, 0), (0, 0, 1), 0.0,FILE_PREFIX+ '_1.json'),
+        ((1, -2, 3), (2.0, 0, 1.0), 0.0,FILE_PREFIX+'_1.json'), 
+        ((1, -2, 3), (2.0, 1.0, 1.0), 0.0,FILE_PREFIX+'_1.json'), 
+    ]
+
 
     @pytest.mark.parametrize('translation, direction, rotation, path',
                              TESTDATA)
     def test_generate_geometry(self,
-                               translation,
-                               direction,
                                rotation,
+                               direction,
+                               translation,
                                path) -> None:
-        electrode = AbbottStjudeActiveTip6142_6145(translation,
+        electrode = AbbottStjudeActiveTip6142_6145(rotation,
                                                    direction,
-                                                   rotation)
+                                                   translation)
         desired = self.load_json(path=path)
         geometry = electrode.generate_geometry()
         assert desired == self.electrode_to_dict(geometry)
+    @pytest.mark.parametrize('translation, direction, rotation, path',
+                             WRONG_TESTDATA)
+    def test_generate_geometry_falseassertion(self,
+                               rotation,
+                               direction,
+                               translation,
+                               path) -> None:
+        electrode = AbbottStjudeActiveTip6142_6145(rotation,
+                                                   direction,
+                                                   translation)
+        desired = self.load_json(path=path)
+        geometry = electrode.generate_geometry()
+        assert desired != self.electrode_to_dict(geometry)
 
     def test_generate_geometry_default(self):
         electrode = AbbottStjudeActiveTip6142_6145()
-        desired = self.load_json(path='case1.json')
+        desired = self.load_json(path=self.FILE_PREFIX+'_0.json')
         geometry = electrode.generate_geometry()
         assert desired == self.electrode_to_dict(geometry)
