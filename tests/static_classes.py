b616ed36f5f0a5251e278412d4f56817eda81f91
Johannes Reding <johannes.reding@uni-rostock.de>
2023-01-09 17:30:10 +0100

    Unittests for electrodes - Refactoring Pt. 1
    Former-commit-id: 5a87811e9b74b7731fc65cebf29f95286c5cb3ce

--- tests/static_classes.py
+++
@@ -1,9 +0,0 @@
-from json import JSONEncoder 
-import dataclasses
-
-# Dataclass JSONEncoder, special behaviour if object is a dataclass object
-class EnhancedJSONEncoder(JSONEncoder):
-        def default(self, o):
-            if dataclasses.is_dataclass(o):
-                return dataclasses.asdict(o)
-            return super().default(o)
--- tests/test_abbottstjudeactivetip6142.py
+++ tests/test_abbottstjudeactivetip6142.py
@@ -1,30 +1,37 @@
 from ossdbs.electrodes.abbott_stjude_active_tip_6142_6145 import AbbottStjudeActiveTip6142_6145
 import pytest
-from tests.electrode_testbase import ElectrodeTestBase
+import netgen
+import json
+
+from tests.test_tom import GeometryConverter
 
 
-#@pytest.mark.skip
-class TestAbbottStJudeActiveTip6142_6145(ElectrodeTestBase):
-    FILE_PREFIX="tests/test_data/AbbottStjudeActiveTip6142_6145"
+# @pytest.mark.skip
+class TestAbbottStJudeActiveTip6142_6145():
+    FILE_PREFIX = "tests/test_data/AbbottStjudeActiveTip6142_6145"
+
+    def geometry_to_dictionary(self,
+                               geometry: netgen.libngpy._NgOCC.TopoDS_Shape
+                               ) -> dict:
+        return GeometryConverter().to_dictionary(geometry)
+
+    def load_json(self, path: str) -> dict:
+        with open(path, "r") as file:
+            geometry_dict = json.load(file)
+        return geometry_dict
+
     TESTDATA = [
-        ((0, 0, 0), (0, 0, 1), 0.0,FILE_PREFIX+ '_0.json'),
-        ((0, 0, 0), (0, 0, 0), 0.0,FILE_PREFIX+ '_0.json'),
-        ((0, 0, 0), (0, 0, 1), 3.0,FILE_PREFIX+ '_0.json'),
-        ((1, -2, 3), (0, 0, 1), 0.0,FILE_PREFIX+ '_1.json'),
-        ((1, -2, 3), (0, 0, 0), 0.0,FILE_PREFIX+ '_1.json'),
-        ((1, -2, 3), (0, 0, 0), 3.0,FILE_PREFIX+ '_1.json'),
-        ((1, -2, 3), (2.0, 0, 1.0), 0.0,FILE_PREFIX+'_2.json'), 
+        ((0, 0, 0), (0, 0, 1), 0.0, FILE_PREFIX + '_0.json'),
+        ((0, 0, 0), (0, 0, 0), 0.0, FILE_PREFIX + '_0.json'),
+        ((0, 0, 0), (0, 0, 1), 3.0, FILE_PREFIX + '_0.json'),
+        ((1, -2, 3), (0, 0, 1), 0.0, FILE_PREFIX + '_1.json'),
+        ((1, -2, 3), (0, 0, 0), 0.0, FILE_PREFIX + '_1.json'),
+        ((1, -2, 3), (0, 0, 0), 3.0, FILE_PREFIX + '_1.json'),
+        ((1, -2, 3), (2.0, 0, 1.0), 0.0, FILE_PREFIX+'_2.json'),
         # Behaviour depends on python handling the floating point operations
-        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 0.0,FILE_PREFIX+'_2.json'),
-        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 3.0,FILE_PREFIX+'_2.json'),
+        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 0.0, FILE_PREFIX+'_2.json'),
+        ((1, -2, 3), (2.0/3.0, 0, 1.0/3.0), 3.0, FILE_PREFIX+'_2.json'),
     ]
-    WRONG_TESTDATA = [
-        ((0, 0, -1), (0, 0, 0), 0.0,FILE_PREFIX+ '_0.json'),
-        ((0, 0, 0), (0, 0, 1), 0.0,FILE_PREFIX+ '_1.json'),
-        ((1, -2, 3), (2.0, 0, 1.0), 0.0,FILE_PREFIX+'_1.json'), 
-        ((1, -2, 3), (2.0, 1.0, 1.0), 0.0,FILE_PREFIX+'_2.json'), 
-    ]
-
 
     @pytest.mark.parametrize('translation, direction, rotation, path',
                              TESTDATA)
@@ -38,23 +45,10 @@ class TestAbbottStJudeActiveTip6142_6145(ElectrodeTestBase):
                                                    translation)
         desired = self.load_json(path=path)
         geometry = electrode.generate_geometry()
-        assert desired == self.electrode_to_dict(geometry)
-    @pytest.mark.parametrize('translation, direction, rotation, path',
-                             WRONG_TESTDATA)
-    def test_generate_geometry_falseassertion(self,
-                               rotation,
-                               direction,
-                               translation,
-                               path) -> None:
-        electrode = AbbottStjudeActiveTip6142_6145(rotation,
-                                                   direction,
-                                                   translation)
-        desired = self.load_json(path=path)
-        geometry = electrode.generate_geometry()
-        assert desired != self.electrode_to_dict(geometry)
+        assert desired == self.geometry_to_dictionary(geometry)
 
     def test_generate_geometry_default(self):
         electrode = AbbottStjudeActiveTip6142_6145()
         desired = self.load_json(path=self.FILE_PREFIX+'_0.json')
         geometry = electrode.generate_geometry()
-        assert desired == self.electrode_to_dict(geometry)
+        assert desired == self.geometry_to_dictionary(geometry)
