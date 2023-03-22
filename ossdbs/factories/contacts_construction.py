d7167750b0e05cea65dec681a27d34362bcb9753
johannes <burg@MacBook-Air-von-Johannes.local>
2023-03-22 09:54:28 +0100

    Bug Fix - ROI and MRI Intersection
    Former-commit-id: 85652d253d0fc00f961cd37b44d9b48ecb2fa2e9

--- ossdbs/factories/contacts_construction.py
+++ ossdbs/factories/contacts_construction.py
@@ -12,21 +12,16 @@ class ContactsFactory:
     def create(self, case_grounding: dict) -> Contacts:
         name = 'BrainSurface'
         active = case_grounding['Active']
-        floating = case_grounding['Floating'] and not active
+        floating = False
         current = case_grounding['Current[A]']
         voltage = case_grounding['Voltage[V]']
-        real = case_grounding['SurfaceImpedance[Ωm]']['real']
-        imag = case_grounding['SurfaceImpedance[Ωm]']['imag']
-        surface_impedance = real + 1j * imag
+        surface_impedance = 0.0 + 0.0j
         contact = Contact(name=name,
                           active=active,
                           floating=floating,
                           current=current,
                           voltage=voltage,
                           surface_impedance=surface_impedance)
-
-        if not contact.active:
-            return self.__electrodes.contacts()
 
         contacts = self.__electrodes.contacts()
         return Contacts(contacts.active() + contacts.floating() + [contact])
