bee8856b90acd6296a52b20fd0d12d3a2c40ae33
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2023-09-19 12:38:15 +0200

    install converter, too

--- leaddbsinterface/convert_input_dictionary.py
+++ leaddbsinterface/convert_input_dictionary.py
@@ -1,28 +1,42 @@
 import argparse
 import json
-from .lead_settings import LeadSettings
+
 from .default_settings import load_default_for_lead
+from .lead_settings import LeadSettings
 
 
 def main():
-    """Lead/OSS Interface
-
-    """
-    parser = argparse.ArgumentParser(prog="Lead-DBS to OSS-DBS converter",
-                                     description="""Converts mat-files written by Lead-DBS
-                                                    to OSS-DBS v2 input dictionaries.""")
-    parser.add_argument('leaddbs_dictionary', type=str,
-                        help="input dictionary in mat format provided by Lead-DBS"
-                        )
-    parser.add_argument('--hemi_side', type=int, choices=[0, 1], required=True,
-                        help="hemisphere side, 0 is right hemisphere, 1 is left hemisphere")
-    parser.add_argument('--output_path', type=str, default="",
-                        help="specify where to store the outputs of the OSS-DBS run")
+    """Lead-DBS/OSS-DBS Interface."""
+    parser = argparse.ArgumentParser(
+        prog="Lead-DBS to OSS-DBS converter",
+        description="""Converts mat-files written by Lead-DBS
+                                                    to OSS-DBS input dictionaries.""",
+    )
+    parser.add_argument(
+        "leaddbs_dictionary",
+        type=str,
+        help="input dictionary in mat format provided by Lead-DBS",
+    )
+    parser.add_argument(
+        "--hemi_side",
+        type=int,
+        choices=[0, 1],
+        required=True,
+        help="hemisphere side, 0 is right hemisphere, 1 is left hemisphere",
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default="",
+        help="specify where to store the outputs of the OSS-DBS run",
+    )
     args = parser.parse_args()
 
     # get settings from oss-dbs_parameters.mat
     ls = LeadSettings(args.leaddbs_dictionary)
-    settings = ls.make_oss_settings(hemis_idx=args.hemi_side, output_path=args.output_path)
+    settings = ls.make_oss_settings(
+        hemis_idx=args.hemi_side, output_path=args.output_path
+    )
 
     # add default settings (alternatively, set using GUI)
     settings = load_default_for_lead(settings)
@@ -36,5 +50,5 @@ def main():
         outfile.write(json_settings)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
