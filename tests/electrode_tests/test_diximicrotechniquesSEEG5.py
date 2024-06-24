2281b8d3c2abe9472f82b9edbf00bf57bb4557de
Jan Philipp Payonk <jan.payonk@uni-rostock.de>
2024-06-24 21:26:12 +0200

    Electrode tests (#37)
    * update electrode tests
    * no meshing in tests
    
    ---------
    
    Co-authored-by: Kenichi Maeda <99521821+kenichi-maeda@users.noreply.github.com>
    Co-authored-by: Julius Zimmermann <julius.zimmermann@uni-rostock.de>

---
+++ tests/electrode_tests/test_diximicrotechniquesSEEG5.py
@@ -0,0 +1,31 @@
+import pytest
+
+from ossdbs.electrodes import DixiSEEG5
+
+from .test_electrodes import TestElectrode
+
+
+class TestDixiSEEG5(TestElectrode):
+    @pytest.fixture
+    def electrode(self):
+        return DixiSEEG5()
+
+    @pytest.fixture
+    def electrode_name(self):
+        return "DixiSEEG5"
+
+    def test_rename_boundaries(self, electrode, electrode_name):
+        """Test whether set_contact_names() works."""
+        self.check_rename_boundaries(electrode, electrode_name)
+
+    def test_contacts(self, electrode, electrode_name):
+        """Test the number and names of contacts."""
+        self.check_contacts(electrode, electrode_name)
+
+    def test_electrode_volume(self, electrode, electrode_name):
+        """Test volume of the entire electrode."""
+        self.check_electrode_volume(electrode, electrode_name)
+
+    def test_contacts_volume(self, electrode, electrode_name):
+        """Test volume of all the contacts."""
+        self.check_contacts_volume(electrode, electrode_name)
