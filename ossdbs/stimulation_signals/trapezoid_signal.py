02c2aa0801a38b688b751a10071d87e06727a7d5
Kenichi Maeda <Kenichi.Maeda001@umb.edu>
2024-06-27 09:39:56 +0200

    implement traiangle and trapezoid signals

--- ossdbs/stimulation_signals/trapezoid_signal.py
+++ ossdbs/stimulation_signals/trapezoid_signal.py
@@ -1,6 +1,8 @@
 # Copyright 2023, 2024 Julius Zimmermann
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import numpy as np
+
 from .signal import TimeDomainSignal
 
 
@@ -30,6 +32,70 @@ class TrapezoidSignal(TimeDomainSignal):
         top_width: float,
         counter_pulse_width: float,
         inter_pulse_width: float,
+        counter_pulse_amplitude: float,
     ) -> None:
         self._top_width = top_width
-        super().__init__(frequency, pulse_width, inter_pulse_width, counter_pulse_width)
+        super().__init__(
+            frequency,
+            pulse_width,
+            inter_pulse_width,
+            counter_pulse_width,
+            counter_pulse_amplitude,
+        )
+
+    def get_time_domain_signal(self, dt: float, timesteps: int) -> np.ndarray:
+        """Build time domain signal."""
+        if np.isclose(dt, 0.0):
+            return np.array([0])
+        if np.isclose(self._top_width, 0.0):
+            return np.zeros(timesteps)
+
+        signal = np.zeros(timesteps)
+        period = 1.0 / self.frequency
+        # use offset for visualization
+        offset = round(self._pulse_width / dt)
+        while offset < timesteps:
+            self._generate_time_domain_signal(
+                signal, offset, self._pulse_width, self._top_width, self.amplitude, dt
+            )
+            if not np.isclose(self._counter_pulse_width, 0.0):
+                counter_pulse_start_idx = offset + round(
+                    self._pulse_width / dt + self._inter_pulse_width / dt
+                )
+                self._generate_time_domain_signal(
+                    signal,
+                    counter_pulse_start_idx,
+                    self._counter_pulse_width,
+                    self._top_width,
+                    -self._counter_amplitude,
+                    dt,
+                )
+            offset += round(period / dt)
+        return signal
+
+    def _generate_time_domain_signal(
+        self,
+        signal: np.array,
+        start_index: int,
+        width: float,
+        top_width: float,
+        amplitude: float,
+        dt: float,
+    ):
+        pulse_duration = round(width / dt)
+        top_duration = round(top_width / dt)
+        pulse_slope_duration = (pulse_duration - top_duration) // 2
+
+        for i in range(pulse_slope_duration):
+            if start_index + i < len(signal):
+                signal[start_index + i] = amplitude * (i / pulse_slope_duration)
+
+        for i in range(top_duration):
+            if start_index + pulse_slope_duration + i < len(signal):
+                signal[start_index + pulse_slope_duration + i] = amplitude
+
+        for i in range(pulse_slope_duration):
+            if start_index + pulse_slope_duration + top_duration + i < len(signal):
+                signal[
+                    start_index + pulse_slope_duration + top_duration + i
+                ] = amplitude * (1 - (i / pulse_slope_duration))
--- ossdbs/stimulation_signals/triangle_signal.py
+++ ossdbs/stimulation_signals/triangle_signal.py
@@ -1,10 +1,69 @@
 # Copyright 2023, 2024 Julius Zimmermann
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import numpy as np
+
 from .signal import TimeDomainSignal
 
 
 class TriangleSignal(TimeDomainSignal):
-    """Represents triangular signal.
-    TODO implement.
+    """Represents a triangular signal.
+
+    Parameters
+    ----------
+    frequency : float
+        Frequency [Hz] of the signal.
+    pulse_width : float
+        Relative pulse width of one period.
+    counter_pulse_width: float
+        Relative width of counter pulse of one period.
+    inter_pulse_width: float
+        Relative width between pulse and counter pulse of one period.
     """
+
+    def get_time_domain_signal(self, dt: float, timesteps: int) -> np.ndarray:
+        """Build time domain signal."""
+        if np.isclose(dt, 0.0):
+            return np.array([0])
+        if np.isclose(self._pulse_width, 0.0):
+            return np.zeros(timesteps)
+
+        signal = np.zeros(timesteps)
+        period = 1.0 / self.frequency
+        # use offset for visualization
+        offset = round(self._pulse_width / dt)
+
+        while offset < timesteps:
+            self._generate_time_domain_signal(
+                signal, offset, self._pulse_width, self.amplitude, dt
+            )
+            if not np.isclose(self._counter_pulse_width, 0.0):
+                counter_pulse_start_index = offset + round(
+                    self._pulse_width / dt + self._inter_pulse_width / dt
+                )
+                self._generate_time_domain_signal(
+                    signal,
+                    counter_pulse_start_index,
+                    self._counter_pulse_width,
+                    -self._counter_amplitude,
+                    dt,
+                )
+
+            offset += round(period / dt)
+        return signal
+
+    def _generate_time_domain_signal(
+        self,
+        signal: np.array,
+        start_index: int,
+        width: float,
+        amplitude: float,
+        dt: float,
+    ):
+        pulse_duration = round(width / dt)
+        for i in range(pulse_duration):
+            if start_index + i < len(signal):
+                if i <= pulse_duration / 2:
+                    signal[start_index + i] = amplitude * (i / pulse_duration) * 2
+                else:
+                    signal[start_index + i] = amplitude * (1 - (i / pulse_duration)) * 2
