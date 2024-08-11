93571efdb3aa537ae75a756e8b8c59dbf3b30daa
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2024-08-11 02:33:26 +0200

    update scripts for plots

--- examples/Approximations/has_sigma_changed.py
+++ examples/Approximations/has_sigma_changed.py
@@ -9,13 +9,15 @@ from ossdbs.stimulation_signals import (
 )
 from ossdbs.utils import have_dielectric_properties_changed
 
+# whether to show plots
+show = False
 frequency = 130
 pulse_width = 60e-6
 inter_pulse_width = 0.0
 counter_pulse_width = 0.0
 
 cutoff_frequency = 1e6
-threshold = 0.05
+threshold = 0.01
 
 # get frequencies
 signal = RectangleSignal(frequency, pulse_width, inter_pulse_width, counter_pulse_width)
@@ -118,99 +120,159 @@ for idx, frequency in enumerate(frequencies):
         ].complex_conductivity(2.0 * np.pi * frequency)
 
 
-for material in material_modelCC4.keys():
-    if material == "CSF":
-        continue
-    # Plotting conductivity
-    plt.plot(frequencies, conductivitiesCC3[material].real, label=material)
-    plt.plot(frequencies, conductivitiesCC3needed[material].real, "o", color="green")
-    plt.plot(frequencies, conductivitiesCC3notneeded[material].real, "o", color="red")
-plt.yscale("log")
+omega = 2.0 * np.pi * frequencies
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.set_yscale("log")
+ax2.set_yscale("log")
 plt.xscale("log")
-plt.xlabel("Frequency / Hz")
-plt.ylabel(r"Real part of conductivity / S m$^{-1}$")
-plt.legend()
-plt.tight_layout()
-plt.show()
-
 for material in material_modelCC4.keys():
     if material == "CSF":
         continue
     # Plotting conductivity
-    plt.plot(frequencies, conductivitiesCC4[material].real, label=material)
-    plt.plot(frequencies, conductivitiesCC4needed[material].real, "o", color="green")
-    plt.plot(frequencies, conductivitiesCC4notneeded[material].real, "o", color="red")
-plt.yscale("log")
-plt.xscale("log")
-plt.xlabel("Frequency / Hz")
-plt.ylabel(r"Real part of conductivity / S m$^{-1}$")
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-plt.title("Compare real and imaginary part CC3")
-for material in material_modelCC4.keys():
-    if material == "CSF":
-        continue
-    # Plotting conductivity
-    plt.plot(
-        frequencies, conductivitiesCC3[material].real, label=material + " real part"
+    ax1.plot(frequencies, conductivitiesCC3[material].real, label=material)
+    ax1.plot(frequencies, conductivitiesCC3needed[material].real, "o", color="green")
+    ax1.plot(frequencies, conductivitiesCC3notneeded[material].real, "o", color="red")
+    ax2.plot(frequencies, conductivitiesCC3[material].imag / omega / e0, ls="dashed")
+    ax2.plot(
+        frequencies,
+        conductivitiesCC3needed[material].imag / omega / e0,
+        "o",
+        color="green",
     )
-    plt.plot(frequencies, conductivitiesCC3needed[material].real, "o", color="green")
-    plt.plot(frequencies, conductivitiesCC3notneeded[material].real, "o", color="red")
+    ax2.plot(
+        frequencies,
+        conductivitiesCC3notneeded[material].imag / omega / e0,
+        "o",
+        color="red",
+    )
 
+plt.xscale("log")
+plt.xlabel("Frequency / Hz")
+ax1.set_ylabel(r"Real part of conductivity / S m$^{-1}$")
+ax2.set_ylabel("Rel. permittivity")
+ax1.legend(loc="upper center")
+plt.tight_layout()
+plt.savefig("CC3_dielectric_properties.pdf")
+if show:
+    plt.show()
+else:
+    plt.close()
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.set_yscale("log")
+ax2.set_yscale("log")
+plt.xscale("log")
+for material in material_modelCC4.keys():
+    if material == "CSF":
+        continue
+    # Plotting conductivity
+    ax1.plot(frequencies, conductivitiesCC4[material].real, label=material)
+    ax1.plot(frequencies, conductivitiesCC4needed[material].real, "o", color="green")
+    ax1.plot(frequencies, conductivitiesCC4notneeded[material].real, "o", color="red")
+    # Plot permittivity
+    ax2.plot(frequencies, conductivitiesCC4[material].imag / omega / e0, ls="dashed")
+    ax2.plot(
+        frequencies,
+        conductivitiesCC4needed[material].imag / omega / e0,
+        "o",
+        color="green",
+    )
+    ax2.plot(
+        frequencies,
+        conductivitiesCC4notneeded[material].imag / omega / e0,
+        "o",
+        color="red",
+    )
+
+plt.yscale("log")
+plt.xscale("log")
+plt.xlabel("Frequency / Hz")
+ax1.set_ylabel(r"Real part of conductivity / S m$^{-1}$")
+ax2.set_ylabel("Rel. permittivity")
+ax1.legend(loc="upper center")
+plt.tight_layout()
+plt.savefig("CC4_dielectric_properties.pdf")
+if show:
+    plt.show()
+else:
+    plt.close()
+
+print("Compare real and imaginary part CC3")
+for material in material_modelCC4.keys():
+    if material == "CSF":
+        continue
+    # Plotting conductivity
     plt.plot(
         frequencies,
-        np.abs(conductivitiesCC3[material].imag),
-        "-",
-        label=material + " imag. part",
+        conductivitiesCC3[material].real / np.abs(conductivitiesCC3[material].imag),
+        label=material,
     )
     plt.plot(
-        frequencies, np.abs(conductivitiesCC3needed[material].imag), "v", color="green"
+        frequencies,
+        conductivitiesCC3needed[material].real
+        / np.abs(conductivitiesCC3needed[material].imag),
+        "o",
+        color="green",
     )
     plt.plot(
-        frequencies, np.abs(conductivitiesCC3notneeded[material].imag), "v", color="red"
+        frequencies,
+        conductivitiesCC3notneeded[material].real
+        / np.abs(conductivitiesCC3notneeded[material].imag),
+        "o",
+        color="red",
     )
 plt.yscale("log")
 plt.xscale("log")
 plt.xlabel("Frequency / Hz")
-plt.ylabel(r"Conductivity / S m$^{-1}$")
+plt.ylabel("Ratio between real and imaginary part")
 plt.legend()
 plt.tight_layout()
-plt.show()
+plt.savefig("CC3_ratios_real_imag.pdf")
+if show:
+    plt.show()
+else:
+    plt.close()
 
 
-plt.title("Compare real and imaginary part CC4")
+print("Compare real and imaginary part CC4")
 for material in material_modelCC4.keys():
     if material == "CSF":
         continue
     # Plotting conductivity
     plt.plot(
-        frequencies, conductivitiesCC4[material].real, label=material + " real part"
+        frequencies,
+        conductivitiesCC4[material].real / np.abs(conductivitiesCC4[material].imag),
+        label=material + " real part",
     )
-    plt.plot(frequencies, conductivitiesCC4needed[material].real, "o", color="green")
-    plt.plot(frequencies, conductivitiesCC4notneeded[material].real, "o", color="red")
-
     plt.plot(
         frequencies,
-        np.abs(conductivitiesCC4[material].imag),
-        "-",
-        label=material + " imag. part",
+        conductivitiesCC4needed[material].real
+        / np.abs(conductivitiesCC4needed[material].imag),
+        "o",
+        color="green",
     )
     plt.plot(
-        frequencies, np.abs(conductivitiesCC4needed[material].imag), "v", color="green"
-    )
-    plt.plot(
-        frequencies, np.abs(conductivitiesCC4notneeded[material].imag), "v", color="red"
+        frequencies,
+        conductivitiesCC4notneeded[material].real
+        / np.abs(conductivitiesCC4notneeded[material].imag),
+        "o",
+        color="red",
     )
 plt.yscale("log")
 plt.xscale("log")
 plt.xlabel("Frequency / Hz")
-plt.ylabel(r"Conductivity / S m$^{-1}$")
+plt.ylabel("Ratio between real and imaginary part")
 plt.legend()
 plt.tight_layout()
-plt.show()
+plt.savefig("CC4_ratios_real_imag.pdf")
+if show:
+    plt.show()
+else:
+    plt.close()
 
+"""
 for material in material_modelCC4.keys():
     plt.plot(
         frequencies[1:],
@@ -236,75 +298,4 @@ plt.ylabel("Relative difference")
 plt.xscale("log")
 plt.xlabel("Frequency / Hz")
 plt.show()
-
-omega = 2.0 * np.pi * frequencies
-for material in material_modelCC4.keys():
-    plt.plot(frequencies, conductivitiesCC3[material].imag / omega / e0, label="CC3")
-    plt.plot(
-        frequencies,
-        conductivitiesCC3needed[material].imag / omega / e0,
-        "o",
-        color="green",
-    )
-    plt.plot(
-        frequencies,
-        conductivitiesCC3notneeded[material].imag / omega / e0,
-        "o",
-        color="red",
-    )
-plt.yscale("log")
-plt.xscale("log")
-plt.xlabel("Frequency / Hz")
-plt.ylabel(r"Rel. permittivity")
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-omega = 2.0 * np.pi * frequencies
-for material in material_modelCC4.keys():
-    plt.plot(frequencies, conductivitiesCC4[material].imag / omega / e0, label="CC4")
-    plt.plot(
-        frequencies,
-        conductivitiesCC4needed[material].imag / omega / e0,
-        "o",
-        color="green",
-    )
-    plt.plot(
-        frequencies,
-        conductivitiesCC4notneeded[material].imag / omega / e0,
-        "o",
-        color="red",
-    )
-plt.yscale("log")
-plt.xscale("log")
-plt.xlabel("Frequency / Hz")
-plt.ylabel(r"Rel. permittivity")
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-for material in material_modelCC4.keys():
-    plt.plot(
-        frequencies[1:],
-        np.abs(np.diff(conductivitiesCC3[material].imag))
-        / np.abs(conductivitiesCC3[material][0:-1]),
-        label=material,
-    )
-plt.legend()
-plt.ylabel("Relative difference")
-plt.xscale("log")
-plt.xlabel("Frequency / Hz")
-plt.show()
-
-for material in material_modelCC4.keys():
-    plt.plot(
-        frequencies[1:],
-        np.abs(np.diff(conductivitiesCC4[material].imag))
-        / np.abs(conductivitiesCC4[material][0:-1]),
-        label=material,
-    )
-plt.legend()
-plt.ylabel("Relative difference")
-plt.xscale("log")
-plt.xlabel("Frequency / Hz")
-plt.show()
+"""
