814271f3ecb705d755fb99f17b2640714edb15cb
JPPayonk <jan.payonk2@uni-rostock.de>
2024-08-13 12:51:41 +0200

    update example for adaptive refinement

--- examples/MeshAPI/BrainVerciseMesh_AdaptiveMeshRefinement.py
+++ examples/MeshAPI/BrainVerciseMesh_AdaptiveMeshRefinement.py
@@ -2,48 +2,110 @@
 Example of a full model geometry
 with one Vercise electrode without
 encapsulation layer.
-The mesh is generated and then
-refined at one surface.
+The mesh is generated and adaptive
+refinement is used.
 """
-from ngsolve import Draw
+import logging
+
+import ngsolve
+from ngsolve import Draw, Redraw
 
 import ossdbs
+from ossdbs.api import (
+    create_bounding_box,
+    generate_electrodes,
+    load_images,
+    prepare_dielectric_properties,
+    prepare_solver,
+    prepare_stimulation_signal,
+    prepare_volume_conductor_model,
+    run_volume_conductor_model,
+    set_contact_and_encapsulation_layer_properties,
+)
+from ossdbs.fem import ConductivityCF
+from ossdbs.model_geometry import BrainGeometry, ModelGeometry
+from ossdbs.utils.settings import Settings
 
-settings = {
+ossdbs.set_logger(logging.INFO)
+
+input_settings = {
+    "BrainRegion": {
+        "Center": {"x[mm]": -9.48, "y[mm]": 11.61, "z[mm]": 4.68},
+        "Dimension": {"x[mm]": 40.0, "y[mm]": 40.0, "z[mm]": 40.0},
+        "Shape": "Ellipsoid",
+    },
     "Electrodes": [
         {
             "Name": "BostonScientificVercise",
             "Rotation[Degrees]": 0,
             "Direction": {"x[mm]": 0, "y[mm]": 0, "z[mm]": 1},
-            "TipPosition": {"x[mm]": 0, "y[mm]": 0, "z[mm]": 0},
-            "EncapsulationLayer": {
-                "Thickness[mm]": 0.0,  # indicates that no encapsulation is modelled
-            },
+            "TipPosition": {"x[mm]": -9.48, "y[mm]": 11.61, "z[mm]": 4.68},
+            "EncapsulationLayer": {"Thickness[mm]": 0.0},
             "Contacts": [
                 {
                     "Contact_ID": 1,
                     "Active": True,
+                    "Voltage[V]": 1.0,
                 },
-                {"Contact_ID": 2, "Active": True},
+                {
+                    "Contact_ID": 3,
+                    "Active": True,
+                    "Voltage[V]": 0.0,
+                },
             ],
         },
     ],
-    "MaterialDistribution": {"MRIPath": "../BrainGeometryAPI/segmask.nii.gz"},
-    "BrainRegion": {
-        "Center": {"x[mm]": 5, "y[mm]": 14, "z[mm]": -4.5},
-        "Dimension": {"x[mm]": 50.0, "y[mm]": 50.0, "z[mm]": 50.0},
-        "Shape": "Ellipsoid",
-    },
+    "MaterialDistribution": {"MRIPath": "../../input_files/Butenko_segmask.nii.gz"},
     "Mesh": {
-        "LoadMesh": False,
-        "SaveMesh": False,
-        "AdaptiveMeshRefinement": {"Active": True, "MaxIterations": 1},
+        "AdaptiveMeshRefinement": {"Active": True, "MaxIterations": 2},
+        "HPRefinement": {"Active": False, "Order": 1},
     },
-    "ExportElectrode": False,
+    "StimulationSignal": {
+        "Type": "Multisine",
+        "ListOfFrequencies": [1000.0],
+    },
+    "ComputeImpedance": True,
 }
 
+settings = Settings(input_settings).complete_settings()
+
+mri_image, dti_image = load_images(settings)
+
+electrodes = generate_electrodes(settings)
+
+brain_region = create_bounding_box(settings["BrainRegion"])
+brain_model = BrainGeometry(settings["BrainRegion"]["Shape"], brain_region)
+
+geometry = ModelGeometry(brain_model, electrodes)
+
+set_contact_and_encapsulation_layer_properties(settings, geometry)
+dielectric_properties = prepare_dielectric_properties(settings)
+materials = settings["MaterialDistribution"]["MRIMapping"]
+
+conductivity = ConductivityCF(
+    mri_image,
+    brain_region,
+    dielectric_properties,
+    materials,
+    geometry.encapsulation_layers,
+    complex_data=settings["EQSMode"],
+    dti_image=dti_image,
+)
+
 mesh = ossdbs.generate_mesh(settings)
-print(mesh.boundaries)
-print(mesh.materials)
-print(mesh.ngsolvemesh.ne)
+
+with ngsolve.TaskManager():
+    solver = prepare_solver(settings)
+    volume_conductor = prepare_volume_conductor_model(
+        settings, geometry, conductivity, solver
+    )
+    frequency_domain_signal = prepare_stimulation_signal(settings)
+
+    run_volume_conductor_model(settings, volume_conductor, frequency_domain_signal)
+
+print("Number of elements before refinement:", mesh.ngsolvemesh.ne)
 Draw(mesh.ngsolvemesh)
+input("Hit enter to see refinement")
+print("Number of elements after refinement:", volume_conductor.mesh.ngsolvemesh.ne)
+mesh = volume_conductor.mesh
+Redraw()
