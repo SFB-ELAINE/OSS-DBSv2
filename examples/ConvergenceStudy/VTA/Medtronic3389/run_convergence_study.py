0e1cb35d50d82e60083ce3aa6188842339793675
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2024-09-11 01:05:01 +0200

    update file handler and convergence routine

--- examples/ConvergenceStudy/VTA/Medtronic3389/run_convergence_study.py
+++ examples/ConvergenceStudy/VTA/Medtronic3389/run_convergence_study.py
@@ -3,11 +3,20 @@
 # to mesh size
 
 import json
+import logging
 
 import numpy as np
 
 import ossdbs
 from ossdbs.main import main_run
+
+
+def remove_file_handler(logger):
+    """Remove file handler from logger instance."""
+    for h in logger.handlers:
+        if isinstance(h, logging.FileHandler):
+            logger.removeHandler(h)
+
 
 BASE_CONTACT = {
     "Contact_ID": 1,
@@ -18,6 +27,8 @@ BASE_CONTACT = {
 }
 
 ossdbs.set_logger()
+_logger = logging.getLogger("ossdbs")
+
 electrode_name = "Medtronic3389"
 
 with open("../base_settings.json") as fp:
@@ -45,18 +56,29 @@ base_input_dict["Mesh"]["MeshingHypothesis"]["Type"] = "Default"
 base_input_dict["Mesh"]["MeshingHypothesis"]["MaxMeshSize"] = 1e6
 base_input_dict["OutputPath"] = "Results_VTA_default"
 main_run(base_input_dict)
+remove_file_handler(_logger)
 
 # second refinement level: fine assumption
 base_input_dict["Mesh"]["MeshingHypothesis"]["Type"] = "Fine"
 base_input_dict["OutputPath"] = "Results_VTA_fine"
 main_run(base_input_dict)
+remove_file_handler(_logger)
 
 # third refinement level: very fine assumption
 base_input_dict["Mesh"]["MeshingHypothesis"]["Type"] = "VeryFine"
 base_input_dict["OutputPath"] = "Results_VTA_very_fine"
 main_run(base_input_dict)
+remove_file_handler(_logger)
 
-# fourth refinement: edge refinement
+# fourth refinement: material refinement
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 2
+base_input_dict["OutputPath"] = "Results_VTA_material_refinement"
+main_run(base_input_dict)
+remove_file_handler(_logger)
+# reset material refinement
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 0
+
+# fifth refinement: edge refinement
 base_input_dict["Mesh"]["MeshingHypothesis"]["Type"] = "Default"
 lead_diameter = ossdbs.electrodes.default_electrode_parameters[
     electrode_name
@@ -67,8 +89,9 @@ base_input_dict["Electrodes"][0]["Contacts"][0]["MaxMeshSizeEdge"] = edge_size
 base_input_dict["Electrodes"][0]["Contacts"][1]["MaxMeshSizeEdge"] = edge_size
 base_input_dict["OutputPath"] = "Results_VTA_edge_refinement"
 main_run(base_input_dict)
+remove_file_handler(_logger)
 
-# fifth refinement: more edge refinement
+# sixth refinement: more edge refinement
 base_input_dict["Mesh"]["MeshingHypothesis"]["Type"] = "Default"
 lead_diameter = ossdbs.electrodes.default_electrode_parameters[
     electrode_name
@@ -79,17 +102,31 @@ base_input_dict["Electrodes"][0]["Contacts"][0]["MaxMeshSizeEdge"] = edge_size
 base_input_dict["Electrodes"][0]["Contacts"][1]["MaxMeshSizeEdge"] = edge_size
 base_input_dict["OutputPath"] = "Results_VTA_fine_edge_refinement"
 main_run(base_input_dict)
+remove_file_handler(_logger)
 
-# sixth refinement: edge refinement + limit on voxel size
+# seventh refinement: edge refinement + limit on voxel size
 mri_image, _ = ossdbs.load_images(base_input_dict)
 max_mesh_size = 10.0 * min(mri_image.voxel_sizes)
 print(f"Imposing max mesh size of: {max_mesh_size:.2f}")
+base_input_dict["Mesh"]["MeshingHypothesis"]["MaxMeshSize"] = max_mesh_size
 base_input_dict["OutputPath"] = "Results_VTA_edge_voxel_refinement"
 main_run(base_input_dict)
+remove_file_handler(_logger)
+
+# eigth refinement: material refinement + edge refinement
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 2
+base_input_dict["Mesh"]["MeshingHypothesis"]["MaxMeshSize"] = 1e6
+base_input_dict["OutputPath"] = "Results_VTA_edge_material_refinement"
+main_run(base_input_dict)
+remove_file_handler(_logger)
+# reset material refinement
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 0
 
 # finest level: voxel size + adaptive mesh refinement
 max_mesh_size = min(mri_image.voxel_sizes)
 base_input_dict["Mesh"]["MeshingHypothesis"]["MaxMeshSize"] = max_mesh_size
+# one material refinement step
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 1
 # reset edge size
 edge_size = 1e6
 base_input_dict["Electrodes"][0]["Contacts"][0]["MaxMeshSizeEdge"] = edge_size
