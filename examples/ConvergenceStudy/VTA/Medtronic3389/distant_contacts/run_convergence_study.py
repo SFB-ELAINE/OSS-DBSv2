72e63258b49d4a93bbad68cf0ab8cd8048e84b94
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2024-09-14 18:39:09 +0200

    full layout for convergence study

--- examples/ConvergenceStudy/VTA/Medtronic3389/distant_contacts/run_convergence_study.py
+++ examples/ConvergenceStudy/VTA/Medtronic3389/distant_contacts/run_convergence_study.py
@@ -31,7 +31,7 @@ _logger = logging.getLogger("ossdbs")
 
 electrode_name = "Medtronic3389"
 
-with open("../base_settings.json") as fp:
+with open("../../base_settings.json") as fp:
     base_input_dict = json.load(fp)
 
 # add electrode
@@ -42,7 +42,7 @@ contact_1_dict["Contact_ID"] = 1
 contact_1_dict["Active"] = True
 contact_1_dict["Voltage[V]"] = 1.0
 contact_2_dict = BASE_CONTACT.copy()
-contact_2_dict["Contact_ID"] = 2
+contact_2_dict["Contact_ID"] = 4
 contact_2_dict["Active"] = True
 base_input_dict["Electrodes"][0]["Contacts"].append(contact_1_dict)
 base_input_dict["Electrodes"][0]["Contacts"].append(contact_2_dict)
@@ -104,7 +104,20 @@ base_input_dict["OutputPath"] = "Results_VTA_fine_edge_refinement"
 main_run(base_input_dict)
 remove_file_handler(_logger)
 
-# seventh refinement: edge refinement + limit on voxel size
+# seventh refinement: very fine edge refinement
+base_input_dict["Mesh"]["MeshingHypothesis"]["Type"] = "Default"
+lead_diameter = ossdbs.electrodes.default_electrode_parameters[
+    electrode_name
+].lead_diameter
+perimeter = np.pi * lead_diameter
+edge_size = perimeter / 75.0
+base_input_dict["Electrodes"][0]["Contacts"][0]["MaxMeshSizeEdge"] = edge_size
+base_input_dict["Electrodes"][0]["Contacts"][1]["MaxMeshSizeEdge"] = edge_size
+base_input_dict["OutputPath"] = "Results_VTA_very_fine_edge_refinement"
+main_run(base_input_dict)
+remove_file_handler(_logger)
+
+# eigth refinement: edge refinement + limit on voxel size
 mri_image, _ = ossdbs.load_images(base_input_dict)
 max_mesh_size = 10.0 * min(mri_image.voxel_sizes)
 print(f"Imposing max mesh size of: {max_mesh_size:.2f}")
@@ -113,10 +126,19 @@ base_input_dict["OutputPath"] = "Results_VTA_edge_voxel_refinement"
 main_run(base_input_dict)
 remove_file_handler(_logger)
 
-# eigth refinement: material refinement + edge refinement
+# ninth refinement: material refinement + edge refinement
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 1
+base_input_dict["Mesh"]["MeshingHypothesis"]["MaxMeshSize"] = 1e6
+base_input_dict["OutputPath"] = "Results_VTA_edge_single_material_refinement"
+main_run(base_input_dict)
+remove_file_handler(_logger)
+# reset material refinement
+base_input_dict["Mesh"]["MaterialRefinementSteps"] = 0
+
+# tenth refinement: material refinement + edge refinement
 base_input_dict["Mesh"]["MaterialRefinementSteps"] = 2
 base_input_dict["Mesh"]["MeshingHypothesis"]["MaxMeshSize"] = 1e6
-base_input_dict["OutputPath"] = "Results_VTA_edge_material_refinement"
+base_input_dict["OutputPath"] = "Results_VTA_edge_double_material_refinement"
 main_run(base_input_dict)
 remove_file_handler(_logger)
 # reset material refinement
