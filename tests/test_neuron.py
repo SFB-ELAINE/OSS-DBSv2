e8b15ed93f7db499dcdb957d45201dff55df6b34
Julius Zimmermann <43932404+j-zimmermann@users.noreply.github.com>
2025-08-09 16:35:38 +0200

    add neuron test

--- tests/test_neuron.py
+++ tests/test_neuron.py
@@ -1,3 +1,5 @@
+import h5py
+import numpy as np
 import pytest
 
 from ossdbs.axon_processing.neuron_model import McNeal1976, NeuronSimulator
@@ -16,10 +18,37 @@ pathway_dict_McNeal = {
 }
 
 
+def create_dummy_h5(filepath):
+    td_solution = h5py.File(filepath, "w")
+    mock_signal = np.array([0, 1, 1, 1, 0, 0, 0, 0])
+    td_solution.create_dataset("TimeSteps[s]", data=np.arange(len(mock_signal)) * 1e-6)
+    grp1 = td_solution.create_group("pathway_1")
+    grp1.create_dataset("Status", data=np.zeros(1))
+    sub_group = grp1.create_group("axon0")
+    sub_group.create_dataset("Potential[V]", data=mock_signal)
+    sub_group.create_dataset("Points[mm]", data=np.array([[1, 1, 1]]))
+    print(td_solution["TimeSteps[s]"])
+    print(td_solution["pathway_1"]["axon0"])
+    return td_solution
+
+
+@pytest.fixture
+def h5_file_1_pathway(tmp_path):
+    h5_file_1_pathway = tmp_path / "h5_file_1_pathway.h5"
+    td_solution = create_dummy_h5(h5_file_1_pathway)
+    return td_solution
+
+
 def test_neuron_model(tmp_path):
     with pytest.raises(TypeError):
         _ = NeuronSimulator(pathway_dict, tmp_path)
 
 
-def test_mc_neal_model(tmp_path):
+def test_initialise_mc_neal_model(tmp_path):
     _ = McNeal1976(pathway_dict_McNeal, tmp_path)
+
+
+def test_process_mc_neal_model(tmp_path, h5_file_1_pathway):
+    model = McNeal1976(pathway_dict_McNeal, tmp_path)
+    model.process_pathways(h5_file_1_pathway)
+    h5_file_1_pathway.close()
