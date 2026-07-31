39cf21b7004689201dea10e29b4d4c5f0758f827
Konstantin Butenko <35195806+Kinway25@users.noreply.github.com>
2026-07-31 11:18:31 +0200

    UPD: always use new mesh settings

--- leaddbsinterface/default_settings.py
+++ leaddbsinterface/default_settings.py
@@ -34,10 +34,14 @@ def load_default_for_lead(settings):
         "White matter"
     )
     settings["ExportVTK"] = True
-    settings["Mesh"]["MeshingHypothesis"]["Type"] = "Default"
-    # material refinement
+
+    # mesh settings based on the conv. study
+    settings["Mesh"]["MeshingHypothesis"]["Type"] = "Fine"
     settings["Mesh"]["MaterialRefinementSteps"] = 1
-    # edge refinement is defined in other file depending on lead geometry
+    settings["Mesh"]["HPRefinement"]["Active"] = "True"
+    settings["Mesh"]["HPRefinement"]["Levels"] = 2
+    settings["Mesh"]["HPRefinement"]["Factor"] = 0.125
+
     settings["FEMOrder"] = 2
     settings["ComputeImpedance"] = False
 
